649a127006d315a1c5a0383efcbc1064eaae8fd2
George-Okumu <gokumu12@gmail.com>
2022-04-17 07:44:27 +0300

    completed

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,17 +1,58 @@
-import React from "react";
+import React, { useState} from "react";
 import { v4 as uuid } from "uuid";
 
-function ItemForm(props) {
+function ItemForm({onItemFormSubmit}) {
+
+//   const [name, setName] = useState("");
+//   const [category, setCategory] = useState("Produce");
+
+//   function handleNewIOtemChange(event){
+//      setName(event.target.value);
+//   }
+//   function handleNewCategoryChange(event){
+//     setCategory(event.target.value);
+//  }
+
+  const [formData, setFormData] = useState({
+    name: "",
+    category: "Produce",
+  });
+
+  function handleChange(event) {
+    let name = event.target.name;
+    let value = event.target.value;
+
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  }
+
+ function handleSubmit(event) {
+  event.preventDefault();
+
+  if(formData.name != ""){
+    onItemFormSubmit({
+      id: uuid(),
+      ...formData
+    });
+    window.alert(formData.name + " added to the List");
+
+  }else{
+    window.alert("Kindly fill the name");
+  }
+
+}
   return (
-    <form className="NewItem">
+    <form className="NewItem" onSubmit={handleSubmit}>
       <label>
         Name:
-        <input type="text" name="name" />
+        <input type="text" name="name" value={formData.name} onChange={handleChange} />
       </label>
 
       <label>
         Category:
-        <select name="category">
+        <select name="category" value={formData.category} onChange={handleChange}>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
